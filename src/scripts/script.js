240e287262830f9e34694e349c06754a60883ccf
ViniSouza001 <viniciussouza130705@gmail.com>
2023-12-28 16:37:46 -0300

    Add Stripe.js library and log data from data.json to the console

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,12 @@
+const stripe = Stripe(
+  "pk_live_51OS4DvEixHlr2hL8LrnLan29l69T9BNFXEbH6siEDhiPAa0QsjWyEf3UuR69bgbRNdIUUhKW7V64NsgWciyFWypM008RA0QbyZ"
+);
+
 const takeData = async () => {
   const resp = await fetch("../data/data.json", { method: "GET" });
   const data = await resp.json();
+  console.log({ data });
   return data;
 };
 
-takeData();
+// takeData();
